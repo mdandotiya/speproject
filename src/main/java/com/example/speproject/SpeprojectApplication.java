a36cb59501de99c20182089b45adfd92e963e75b
Manu Dandotiya <iitianmd13@gmail.com>
2021-04-04 11:57:36 +0530

    2nd commit

--- src/main/java/com/example/speproject/SpeprojectApplication.java
+++ src/main/java/com/example/speproject/SpeprojectApplication.java
@@ -4,12 +4,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ScopeMetadata;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class SpeprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpeprojectApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
--- src/main/java/com/example/speproject/rest/AdminRestController.java
+++ src/main/java/com/example/speproject/rest/AdminRestController.java
@@ -3,9 +3,7 @@ package com.example.speproject.rest;
 import com.example.speproject.entity.Admin;
 import com.example.speproject.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
