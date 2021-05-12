f7b75fa9c4d083bec4c8fdba1e7391e267323f1c
Manu Dandotiya <iitianmd13@gmail.com>
2021-05-12 10:02:48 +0530

    Room test case written

--- src/test/java/com/example/speproject/RoomTest.java
+++ src/test/java/com/example/speproject/RoomTest.java
@@ -4,11 +4,17 @@ import com.example.speproject.dao.RoomDao;
 import com.example.speproject.entity.Room;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RoomTest.class)
 @DataJpaTest
 public class RoomTest {
 
