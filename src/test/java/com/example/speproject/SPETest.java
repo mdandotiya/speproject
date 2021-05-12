9ca8997260819ccfd60062fa978f4f7613a4b1c1
Manu Dandotiya <iitianmd13@gmail.com>
2021-05-12 11:48:22 +0530

    CleaningStaff test case written

--- src/test/java/com/example/speproject/SPETest.java
+++ src/test/java/com/example/speproject/SPETest.java
@@ -1,10 +1,11 @@
 package com.example.speproject;
 
 import com.example.speproject.dao.CleaningStaffDao;
+import com.example.speproject.dao.RoomDao;
 import com.example.speproject.entity.CleaningStaff;
-import com.example.speproject.entity.ComplaintStaff;
+import com.example.speproject.entity.Room;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,21 +13,48 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CleaningStaffTest {
+public class SPETest {
+
+    @Autowired
+    private RoomDao roomDao;
 
     @Autowired
     CleaningStaffDao cleaningStaffDao;
 
     @Test
+    public void should_find_All_Rooms(){
+        List<Room> rooms = roomDao.findAll();
+        int noOfRooms = 4;
+        Assert.assertEquals(noOfRooms,rooms.size());
+    }
+
+    @Test
+    public void deleteTest(){
+        Room room = new Room(105,"AC","No");
+        roomDao.save(room);
+        int id=0;
+        List<Room> rooms = roomDao.findAll();
+        for(int i=0;i<rooms.size();i++){
+            if(rooms.get(i).getRoomid() == room.getRoomid()){
+                id = rooms.get(i).getId();
+                roomDao.deleteById(rooms.get(i).getId());
+                break;
+            }
+        }
+        Assert.assertFalse(roomDao.findById(id).isPresent());
+    }
+
+    @org.junit.Test
     public void testAllStaff(){
         List<CleaningStaff> cleaningStaffList = cleaningStaffDao.findAll();
         int noOfStaff = 3;
         Assert.assertEquals(noOfStaff,cleaningStaffList.size());
     }
 
-    @Test
+    @org.junit.Test
     public void deleteStaffTest(){
         CleaningStaff cleaningStaff = new CleaningStaff("4th","Surendra Kumar","TTS");
         cleaningStaffDao.save(cleaningStaff);
@@ -41,4 +69,6 @@ public class CleaningStaffTest {
         }
         Assert.assertFalse(cleaningStaffDao.findById(id).isPresent());
     }
+
+
 }
