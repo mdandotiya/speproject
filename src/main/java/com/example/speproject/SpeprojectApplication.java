911aae00ee20f8950bdda4efc0b0e0fd25f23258
yagnik112358 <ybprajapati96@gmail.com>
2021-05-14 21:42:21 +0530

    Updated allowed crossorigin address.

--- src/main/java/com/example/speproject/SpeprojectApplication.java
+++ src/main/java/com/example/speproject/SpeprojectApplication.java
@@ -11,6 +11,9 @@ import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 import java.util.Arrays;
@@ -23,22 +26,30 @@ public class SpeprojectApplication {
 		SpringApplication.run(SpeprojectApplication.class, args);
 	}
 
+//	@Bean
+//	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration();
+// 		config.setAllowCredentials(true);
+// // 		config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+// 		config.setAllowedOrigins(Arrays.asList("http://172.17.0.2:80"));
+// // 		config.setAllowedOrigins("*");
+// 		config.setAllowedMethods(Collections.singletonList("*"));
+// 		config.setAllowedHeaders(Collections.singletonList("*"));
+//		source.registerCorsConfiguration("/**", config);
+//		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//		return bean;
+//	}
 	@Bean
-	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
- 		config.setAllowCredentials(true);
- // 		config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
- 		config.setAllowedOrigins(Arrays.asList("http://172.17.0.2:80"));
- // 		config.setAllowedOrigins("*");
- 		config.setAllowedMethods(Collections.singletonList("*"));
- 		config.setAllowedHeaders(Collections.singletonList("*"));
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
-
 	@Bean
 	public DataSource getDataSource() {
 		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
